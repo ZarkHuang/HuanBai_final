b79c63c2f00aacea25b9ef96837ea12602f0907f
Muxuan Liu <liumuxuan18@gmail.com>
2023-03-03 11:49:14 +0800

    試推

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css"
-// import "../style/home/home.css";
 import ToTop from "../component/ToTop";
 import Scroll from "../component/Scroll";
 // import NavBar from "../component/NavBar";
